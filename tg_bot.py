076ef17160220b66e116e0c9baf23913b78b7351
Olga <o.panova27@gmail.com>
2023-06-29 22:15:21 +0300

    Add VK bot

--- tg_bot.py
+++ tg_bot.py
@@ -44,20 +44,6 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
 
         return response.query_result.fulfillment_text
 
-def create_api_key(project_id: str, suffix: str) -> Key:
-    client = api_keys_v2.ApiKeysClient()
-
-    key = api_keys_v2.Key()
-    key.display_name = f"My first API key - {suffix}"
-
-    request = api_keys_v2.CreateKeyRequest()
-    request.parent = f"projects/{project_id}/locations/global"
-    request.key = key
-
-    response = client.create_key(request=request).result()
-
-    print(f"Successfully created an API key: {response.name}")
-    return response
 
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
---
+++ vk_bot.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def main() -> None:
+    load_dotenv()
+    vk_group_token = os.getenv("VK_GROUP_TOKEN")
+    
+    vk_session = vk_api.VkApi(token=vk_group_token)
+
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+            if event.to_me:
+                print('Для меня от: ', event.user_id)
+            else:
+                print('От меня для: ', event.user_id)
+            print('Текст:', event.text)
+
+
+if __name__ == '__main__':
+    main()
