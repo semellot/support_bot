32cd6a58411358db53535b0a2b67046cd3fbf03e
Olga <o.panova27@gmail.com>
2023-06-30 08:05:49 +0300

    Add logging

--- tg_bot.py
+++ tg_bot.py
@@ -11,22 +11,24 @@ from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+logger = logging.getLogger('Logger for tg_bot')
 
-logger = logging.getLogger(__name__)
+class TelegramLogsHandler(logging.Handler):
 
-load_dotenv()
-tg_bot_token = os.getenv("TG_BOT_TOKEN")
-google_project_id = os.getenv("GOOGLE_PROJECT_ID")
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    def __init__(self, tg_token, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = telegram.Bot(token=tg_token)
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
     session_client = dialogflow.SessionsClient()
 
     session = session_client.session_path(project_id, session_id)
-    print("Session path: {}\n".format(session))
 
     for text in texts:
         text_input = dialogflow.TextInput(text=text, language_code=language_code)
@@ -34,18 +36,8 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
         query_input = dialogflow.QueryInput(text=text_input)
 
         response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
+            request={'session': session, 'query_input': query_input}
         )
-
-        print("=" * 20)
-        print("Query text: {}".format(response.query_result.query_text))
-        print(
-            "Detected intent: {} (confidence: {})\n".format(
-                response.query_result.intent.display_name,
-                response.query_result.intent_detection_confidence,
-            )
-        )
-        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
 
         return response.query_result.fulfillment_text
 
@@ -64,23 +56,28 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 def echo(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
-    answer = detect_intent_texts(google_project_id, user.id, [update.message.text], "ru-RU")
+    google_project_id = os.environ['GOOGLE_PROJECT_ID']
+    answer = detect_intent_texts(google_project_id, user.id, [update.message.text], 'ru-RU')
     update.message.reply_text(answer)
 
 
 def main() -> None:
+    load_dotenv()
+    tg_bot_token = os.getenv('TG_BOT_TOKEN')
+    os.environ['GOOGLE_PROJECT_ID'] = os.getenv('GOOGLE_PROJECT_ID')
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
     updater = Updater(tg_bot_token)
-
     dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', help_command))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    updater.start_polling()
-
-    updater.idle()
+    try:
+        updater.start_polling()
+        updater.idle()
+    except requests.exceptions.HTTPError as err:
+        logger.warning(f'Ошибка!\n{err}')
 
 
 if __name__ == '__main__':
--- vk_bot.py
+++ vk_bot.py
@@ -11,11 +11,24 @@ import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
+logger = logging.getLogger('Logger for vk_bot')
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_token, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = telegram.Bot(token=tg_token)
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 def detect_intent_texts(project_id, session_id, texts, language_code):
     session_client = dialogflow.SessionsClient()
 
     session = session_client.session_path(project_id, session_id)
-    print("Session path: {}\n".format(session))
 
     for text in texts:
         text_input = dialogflow.TextInput(text=text, language_code=language_code)
@@ -23,25 +36,17 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
         query_input = dialogflow.QueryInput(text=text_input)
 
         response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
+            request={'session': session, 'query_input': query_input}
         )
 
-        print("=" * 20)
-        print("Query text: {}".format(response.query_result.query_text))
-        print(
-            "Detected intent: {} (confidence: {})\n".format(
-                response.query_result.intent.display_name,
-                response.query_result.intent_detection_confidence,
-            )
-        )
-        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
         if response.query_result.intent.is_fallback:
             return False
         else:
             return response.query_result.fulfillment_text
 
+
 def echo(event, vk_api, google_project_id):
-    answer = detect_intent_texts(google_project_id, event.user_id, [event.text], "ru-RU")
+    answer = detect_intent_texts(google_project_id, event.user_id, [event.text], 'ru-RU')
     if answer:
         vk_api.messages.send(
             user_id=event.user_id,
@@ -51,16 +56,20 @@ def echo(event, vk_api, google_project_id):
 
 def main() -> None:
     load_dotenv()
-    vk_group_token = os.getenv("VK_GROUP_TOKEN")
-    google_project_id = os.getenv("GOOGLE_PROJECT_ID")
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    vk_group_token = os.getenv('VK_GROUP_TOKEN')
+    google_project_id = os.getenv('GOOGLE_PROJECT_ID')
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
     vk_session = vk.VkApi(token=vk_group_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api, google_project_id)
+
+    try:
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                echo(event, vk_api, google_project_id)
+    except requests.exceptions.HTTPError as err:
+        logger.warning(f'Ошибка!\n{err}')
 
 
 if __name__ == '__main__':
